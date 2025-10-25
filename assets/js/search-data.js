7d873a760cececbead50f6185feb82aca36e2bb0
YukiShirai <42454182+YukiShirai@users.noreply.github.com>
2025-10-25 16:50:11 +0000

    Deploying to gh-pages from @ YukiShirai/YukiShirai.github.io@9ce80408faf587cab8d254f48733306e29ad7553 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Please click on the icon to download my CV if it does not appear in your browser.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
