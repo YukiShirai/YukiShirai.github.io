1d4653848bb6e2308a0e9f3c93702913b7e1abaa
YukiShirai <42454182+YukiShirai@users.noreply.github.com>
2025-10-25 18:12:34 +0000

    Deploying to gh-pages from @ YukiShirai/YukiShirai.github.io@c1a3d06665a1238f5da534f0cf63920cc2142b10 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -369,16 +369,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-paper-was-accepted-for-neurips-2025-workshop-embodied-world-models-for-decision-making",
+          title: 'Our paper was accepted for NeurIPS 2025 Workshop Embodied World Models for Decision...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
